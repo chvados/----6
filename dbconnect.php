6362813f20621c8efd4a71db310c7812642b9c33
chvados <warface2015_viziter@mail.ru>
2020-11-28 01:32:47 +0700

    First listing

---
+++ dbconnect.php
@@ -0,0 +1,9 @@
+<?php
+$host = 'localhost';
+$database = 'publications';
+$user = 'root';
+$password = '';
+$mysqli = mysqli_connect($host, $user, $password, $database);
+if (mysqli_connect_errno($mysqli)) {
+   echo "Не удалось подключиться к MySQL: " . mysqli_connect_error();
+}
